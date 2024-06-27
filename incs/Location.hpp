9952d9811a4b472d13d75a001ed8007d48ed90c6
Samuel Cordero <sacorder@student.42madrid.com>
2024-06-27 18:10:19 +0200

    ready for logic

--- incs/Location.hpp
+++ incs/Location.hpp
@@ -12,18 +12,19 @@ class Location {
 	private:
 		std::string							index;
 		std::string 						uri;
+		std::string							root;
 		int									flagsMethods;
 		std::pair<std::string, std::string>	cgi;
-		std::string							root;
-	
 	public:
 		Location(std::vector<std::string> locationBlock);
+		Location(const Location &other);
 		~Location();
 		std::string					getUri();
 		std::string					getRoot();
 		int							getMethods();	
-		void						setMethods(size_t i, const std::vector<std::string> &locationBlock);
-		void						setIndex(size_t i, std::vector<std::string> &locationBlock);
-		void						setCgi(size_t i, std::vector<std::string> &locationBlock);
-		void						setRoot(size_t i, std::vector<std::string> &locationBlock);
+		size_t						setMethods(size_t i, const std::vector<std::string> &locationBlock);
+		size_t						setIndex(size_t i, std::vector<std::string> &locationBlock);
+		size_t						setCgi(size_t i, std::vector<std::string> &locationBlock);
+		size_t						setRoot(size_t i, std::vector<std::string> &locationBlock);
+		Location					&operator=(const Location &other);
 };
