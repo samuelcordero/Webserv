bd680245aaa86ba6ba7f06e3a41f241666329a95
Bruno Azuara <bruno.azuara@gmail.com>
2024-07-03 15:41:40 +0200

    first working timout limit

---
+++ CGI/long_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from art import *
+import time
+
+time.sleep(10)
+
+
+Art=text2art("Demo",font='block',chr_ignore=True)
+print(Art)
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -7,9 +7,13 @@ int main()
 
 	// test cgi
 	std::cout << "CGI TEST" << std::endl;
-	CGIHandler cgiHandler("CGI/test.py");
-	cgiHandler.handleRequest();
-	std::cout << "CGI TEST: Begining of output data -> " << cgiHandler.getOutputData() << "<- End of output data." << std::endl;
+	CGIHandler cgiHandler1("CGI/test.py");
+	cgiHandler1.handleRequest();
+	std::cout << "CGI TEST: Begining of output data 1 -> " << cgiHandler1.getOutputData() << "<- End of output data 1." << std::endl;
+
+	CGIHandler cgiHandler2("CGI/long_test.py");
+	cgiHandler2.handleRequest();
+	std::cout << "CGI TEST: Begining of output data 2 -> " << cgiHandler2.getOutputData() << "<- End of output data 2." << std::endl;
 	std::cout << "CGI TEST END" << std::endl;
 	// end of cgi test
 
